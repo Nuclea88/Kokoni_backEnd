777f6e9a1c8e3104dd3295cb45b25b29b92b4264
Nuclea88 <ingrid.lopov@gmail.com>
2026-03-17 10:06:10 +0100

    first commit

---
+++ src/main/java/com/example/kokoni/KokoniApplication.java
@@ -0,0 +1,13 @@
+package com.example.kokoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokoniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KokoniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kokoni/KokoniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kokoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokoniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
