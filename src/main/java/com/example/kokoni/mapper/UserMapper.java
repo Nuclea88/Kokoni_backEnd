41c4c757c38de4ca0b615d346bbd3861c3995ae5
Nuclea88 <ingrid.lopov@gmail.com>
2026-03-17 10:28:16 +0100

    chore: basic structure, added dependencies and security files

---
+++ src/main/java/com/example/kokoni/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.example.kokoni.mapper;
+// package com.example.mapper;
+
+// import org.mapstruct.BeanMapping;
+// import org.mapstruct.Mapper;
+// import org.mapstruct.MappingTarget;
+// import org.mapstruct.NullValuePropertyMappingStrategy;
+// import org.mapstruct.ReportingPolicy;
+
+
+// @Mapper(componentModel ="spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+// public interface UserMapper {
+//     User toEntity (UserRequestDTO dto, byte[] picture);
+
+//     UserBasicResponseDTO toBasicResponse (User user);
+
+//     UserProfileResponseDTO toProfileResponse (User user);
+
+//     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+//     void updateEntityFromDto(UserRequestDTO dto, @MappingTarget User user);
+
+// }
---
+++ src/main/java/com/example/kokoni/security/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,57 @@
+package com.example.kokoni.security.filter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    
+    private final String secret;
+
+    public JWTAuthorizationFilter(String secret){
+        this.secret = secret;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        try{
+        String token = header.replace("Bearer ", "");
+
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(secret))
+            .build()
+            .verify(token);
+
+        String username = decodedJWT.getSubject();
+        int userId = decodedJWT.getClaim("userId").asInt();
+
+        List<GrantedAuthority> authorities = Collections.emptyList();
+           UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        authentication.setDetails(userId);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        }catch (Exception e){
+            SecurityContextHolder.clearContext();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
